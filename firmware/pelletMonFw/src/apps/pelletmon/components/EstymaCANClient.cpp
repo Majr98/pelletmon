1e35f03502709f4a13c4e97887296e9ab18d2b6d
cziter15 <chris.streh@gmail.com>
2021-02-01 21:32:29 +0100

    Had some bugs, fixed them

--- firmware/pelletMonFw/src/apps/pelletmon/components/EstymaCANClient.cpp
+++ firmware/pelletMonFw/src/apps/pelletmon/components/EstymaCANClient.cpp
@@ -1,23 +1,34 @@
 #include "EstymaCANCLient.h"
+#include "../../../board.h"
+#include <CAN.h>
 
 namespace comps
 {
 	EstymaCANCLient::EstymaCANCLient()
 	{
+		CAN.setPins(CAN_RX_PIN, CAN_TX_PIN);
 	}
 
 	bool EstymaCANCLient::init(ksf::ksComposable* owner)
 	{
+		CAN.begin(125E3);
 		return true;
 	}
 
 	bool EstymaCANCLient::loop()
 	{
+		int packetSize = CAN.parsePacket();
+
+		if (packetSize)
+		{
+			//todo
+		}
+
 		return true;
 	}
 
 	EstymaCANCLient::~EstymaCANCLient()
 	{
-
+		CAN.end();
 	}
 }
